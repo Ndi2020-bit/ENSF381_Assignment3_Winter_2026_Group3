039846fb4835ea148570dce69df3481dbbce02bd
Nnamdi Onaga <onaga.nnamdi@gmail.com>
2026-03-29 17:56:31 -0600

    Added images to the other flavors and fixed their names, and also added our names and UCIDs to in index.js

--- sweet-scoop-app/src/index.js
+++ sweet-scoop-app/src/index.js
@@ -1,3 +1,7 @@
+//Name and UCID of Group 3:
+//1. Nnamdi Onaga, 30254264 
+//2. Somrit Dewan, 30230926
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -17,3 +21,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
